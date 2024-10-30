a40c9673eda83bb4732ebd43a06ebb6527de1aca
bruno5barros <67475745+bruno5barros@users.noreply.github.com>
2024-10-30 23:58:47 +0100

    Delete begin directory

--- begin/wpm/Wpm.Web/Domain/Breed.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Wpm.Web.Domain;
-public class Breed
-{
-    public int Id { get; set; }
-    public required string Name { get; set; }
-    public decimal IdealMaxWeight { get; set; }
-    public Species Species { get; set; }
-    public int SpeciesId { get; set; }
-}
